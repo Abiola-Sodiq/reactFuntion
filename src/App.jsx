71081c4dc8cf7e1a2ee9cd3ff33d6bc3d99594f3
abiola-sodiq <rasababe01@gmail.com>
2023-06-07 14:39:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+// import React, { Component } from 'react'
+
+
+// export class App extends Component {
+//   state ={
+//     name:"Abiola Sodiq",
+//     street: "Odogunyan, Ikorodu,Lagos"
+//   }
+
+//   // affidavit =(e) =>{
+//   //   this.setState({
+//   //     name:e.target.value
+//   //   })
+//   // }
+//   render() {
+//     return (
+//       <div>
+//         <h1>My Name is {this.state.name} and i stay at {this.state.street} </h1>
+//         {/* <input type="text" onChange={this.affidavit} /> */}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+// import {useState} from 'react'
+
+// const App = () => {
+//   const [name, setname] = useState("Abibi")
+
+//      const  affidavit =(e) =>{
+//     setname(e.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h1>my name is {name}</h1>
+//       <input type="text" onChange={affidavit} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/component/profile-card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const profileCard = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default profileCard
